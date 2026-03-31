6cda4dab942d829bb2db97152523cbf7f556d78b
VedantKudalkar21 <vedantkudalkar90@gmail.com>
2026-03-31 14:43:32 +0530

    fix middleware issuses on vercel

--- app/api/test-email/route.js
+++ app/api/test-email/route.js
@@ -1,8 +1,23 @@
-// app/api/test-email/route.js
+import arcjet, { detectBot, shield } from "@arcjet/next";
+import { NextResponse } from "next/server";
 
-import { sendEmail } from "@/actions/send-email";
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
 
-export async function GET() {
-  const res = await sendEmail();
-  return Response.json(res);
+export async function GET(req) {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return new NextResponse("Blocked", { status: 403 });
+  }
+
+  return NextResponse.json({ success: true });
 }
